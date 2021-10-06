977e4f718892378d7cd8c30ebe04f06851847f36
Farhod Fozilov <ffozilov@yandex.ru>
2021-10-06 22:10:51 +0300

    Исправить комментарии ревьюера

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -7,7 +7,7 @@ function Navigation({ isOpen }) {
       <React.Fragment>
     <nav className={`navigation${isOpen ? '_visible' : '_hidden'}`}>
       <ul className='navigation__links'>
-          <li className='navigation__list-item'><NavLink exact to='/' activeClassName='navigation__link_active' className='navigation__link page__link-transparency'>Главная</NavLink></li>
+          <li className='navigation__list-item navigation__main-page-link'><NavLink exact to='/' activeClassName='navigation__link_active' className='navigation__link page__link-transparency'>Главная</NavLink></li>
          <li className='navigation__list-item'><NavLink to='/movies' activeClassName='navigation__link_active' className='navigation__link page__link-transparency'>Фильмы</NavLink></li>
           <li className='navigation__list-item'><NavLink to='/saved-movies' activeClassName='navigation__link_active' className='navigation__link page__link-transparency'>Сохраненные фильмы</NavLink></li>
           <li className='navigation__list-item'><NavLink to='/profile' className='profile__account-button page__link-transparency'>Аккаунт</NavLink></li>
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -5,7 +5,7 @@ function SearchForm() {
   return (
         <form className='search-form'>
             <div className='search-form__upper-row-container'>
-                <input className='search-form__input_type_text' placeholder='Фильмы'/>
+                <input className='search-form__input_type_text' placeholder='Фильмы' required={true}/>
                 <button type='submit' className='search-form__button button-hover-transform'>Найти</button>
             </div>
             <FilterCheckbox/>
